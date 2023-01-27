cbbb1b1509a4218afafff514e0aa88fe6b491cdd
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-01-27 11:55:05 +0300

    Update MainApp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -18,32 +18,31 @@ public class MainApp {
       UserService userService = context.getBean(UserService.class);
       CarService carService = context.getBean(CarService.class);
 
-      User user1 = new User("User1","Lastname1", "user1@mail.ru");
-      userService.add(user1);
+
+
       Car car1 = new Car("Volga", 21);
-      carService.add(car1);
-      user1.setCar(car1);
-      car1.setUser(user1);
+      User user1 = new User("User1","Lastname1", "user1@mail.ru", car1);
+      userService.add(user1);
 
 
-      User user2 = new User("User2","Lastname2", "user2@mail.ru");
-      userService.add(user2);
+
+
       Car car2 = new Car("Vaz", 2106);
-      user2.setCar(car2);
-      car2.setUser(user2);
+      User user2 = new User("User2","Lastname2", "user2@mail.ru", car2);
+      userService.add(user2);
 
-      User user3 = new User("User3","Lastname3", "user3@mail.ru");
-      userService.add(user3);
+
+
       Car car3 = new Car("Gaz", 21);
-      user3.setCar(car3);
-      car3.setUser(user3);
+      User user3 = new User("User3","Lastname3", "user3@mail.ru", car3);
+      userService.add(user3);
 
 
-      User user4 = new User("User4","Lastname4", "user4@mail.ru");
-      userService.add(user4);
+
+
       Car car4 = new Car("Vaz", 2109);
-      user4.setCar(car4);
-      car4.setUser(user4);
+      User user4 = new User("User4","Lastname4", "user4@mail.ru", car4);
+      userService.add(user4);
 
 
 
@@ -57,7 +56,8 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
-//      System.out.println(carService.getUserAndCar("Vaz", 2109).toString());
+      System.out.println(userService.getUserCar("Gaz", 21).toString());
       context.close();
    }
 }
+
