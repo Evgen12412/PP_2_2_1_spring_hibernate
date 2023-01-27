9aa1dc1f09bb84c37b7a2d4a3eb17cb593ea9dbe
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-01-27 11:53:40 +0300

    Update UserService.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,12 +1,13 @@
 package hiber.service;
 
-import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
+     User getUserCar(String model, int series);
 
     List<User> listUsers();
 }
