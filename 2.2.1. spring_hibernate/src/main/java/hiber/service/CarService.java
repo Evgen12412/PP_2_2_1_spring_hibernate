04221dbeff018a2ff1dbf2781f15fdfc4d0466f8
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-01-30 08:21:08 +0300

    Update CarService.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
@@ -1,10 +1,9 @@
 package hiber.service;
 
 import hiber.model.Car;
-import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface CarService {
     void add(Car car);
-
-    User getUserAndCar(String model, int series);
 }
