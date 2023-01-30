99d32a4ea9455a0103e58f1a709178c2d7093c0f
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-01-30 08:22:53 +0300

    Update CarServiceImp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.CarDao;
 import hiber.model.Car;
-import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,9 +21,5 @@ public class CarServiceImp implements CarService {
     public void add(Car car) {
         carDao.add(car);
     }
-    @Transactional(readOnly = true)
-    @Override
-    public User getUserAndCar(String model, int series) {
-        return carDao.getUserAndCar(model, series);
-    }
+
 }
