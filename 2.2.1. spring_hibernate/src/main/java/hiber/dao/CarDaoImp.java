1a7536ae3ead4fdb73fe499b17bf0c80bc42b291
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-01-30 08:09:59 +0300

    Update CarDaoImp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
@@ -11,7 +11,6 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 @Repository
-@Transactional
 public class CarDaoImp implements CarDao {
 
     @Autowired
@@ -26,12 +25,4 @@ public class CarDaoImp implements CarDao {
         sessionFactory.getCurrentSession().save(car);
     }
 
-    @Override
-    public User getUserAndCar(String model, int series) {
-//        TypedQuery<User> query = sessionFactory.getCurrentSession()
-//                .createQuery("from User where id = (select id from Car where model = :model and :series)");
-//        query.setParameter("model", model).setParameter("series", series);
-//        return query.setMaxResults(1).getSingleResult();
-        return null;
-    }
 }
